ed2cca26496a658fff91e55664198024ad2ca16f
Anton <dedul.9@yandex.ru>
2021-06-09 16:33:29 +0300

    Added the first command

--- index.js
+++ index.js
@@ -6,7 +6,21 @@ const TOKEN = process.env.TOKEN;
 
 const bot = new TelegramApi(TOKEN, { polling: true });
 
+bot.setMyCommands([
+  { command: '/start', description: 'Start using the Viking Training Bot' }
+])
+
 bot.on("message", (msg) => {
   const chatId = msg.chat.id;
-  bot.sendMessage(chatId, "Hello, friend!");
+  const text = msg.text;
+
+  switch (text) {
+    case "/start":
+      bot.sendMessage(chatId, `Hello, ${msg.from.first_name} ${msg.from.last_name}! Click «Start exercising», choose a muscle group and start work!`);
+      break;
+    default:
+      bot.sendMessage(chatId, "I don’t understand you. Choose some command!");
+      break;
+  }
+
 });
