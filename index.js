ac5ed56952b51ed44fd8376f83666df1e318eb55
Anton <dedul.9@yandex.ru>
2021-06-09 16:34:18 +0300

    Prettier format index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const TOKEN = process.env.TOKEN;
 const bot = new TelegramApi(TOKEN, { polling: true });
 
 bot.setMyCommands([
-  { command: '/start', description: 'Start using the Viking Training Bot' }
-])
+  { command: "/start", description: "Start using the Viking Training Bot" },
+]);
 
 bot.on("message", (msg) => {
   const chatId = msg.chat.id;
@@ -16,11 +16,13 @@ bot.on("message", (msg) => {
 
   switch (text) {
     case "/start":
-      bot.sendMessage(chatId, `Hello, ${msg.from.first_name} ${msg.from.last_name}! Click «Start exercising», choose a muscle group and start work!`);
+      bot.sendMessage(
+        chatId,
+        `Hello, ${msg.from.first_name} ${msg.from.last_name}! Click «Start exercising», choose a muscle group and start work!`
+      );
       break;
     default:
       bot.sendMessage(chatId, "I don’t understand you. Choose some command!");
       break;
   }
-
 });
