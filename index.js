508162c19f1305f9804c1e1e9dfeaad562138a5d
Anton <dedul.9@yandex.ru>
2021-07-02 23:24:47 +0300

    Code refactoring for heroku

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 require("dotenv").config();
 
+const express = require("express");
 const bot = require("./bot");
-require("./web")(bot);
+
+const app = express();
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log("Web server started on port: " + port);
+});
+
+app.post("/" + bot.token, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
