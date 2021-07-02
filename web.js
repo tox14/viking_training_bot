4c293593257264b03b32973fad0483abb028b9c5
Anton <dedul.9@yandex.ru>
2021-07-02 22:36:38 +0300

    Code refactoring for heroku

---
+++ web.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log("Web server started on port: " + port);
+});
+
+module.exports = (bot) => {
+  app.post("/" + bot.token, (req, res) => {
+    bot.processUpdate(req.body);
+    res.sendStatus(200);
+  });
+};
