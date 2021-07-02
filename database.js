b1c4132b6015eb249b5f2fd30f0a79ea525b4c1a
Anton <dedul.9@yandex.ru>
2021-07-02 23:09:58 +0300

    Code refactoring for heroku

--- database.js
+++ database.js
@@ -8,6 +8,12 @@ const sequelize = new Sequelize(
     dialect: "postgres",
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   }
 );
 
