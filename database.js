527fd557a4a8ddce11dcfd05a8d598be5c1664da
Anton <dedul.9@yandex.ru>
2021-09-05 22:04:37 +0300

    Chaged db from postgres to mysgl

--- database.js
+++ database.js
@@ -5,9 +5,8 @@ const sequelize = new Sequelize(
   process.env.DB_USER_LOGIN,
   process.env.DB_USER_PASSWORD,
   {
-    dialect: "postgres",
+    dialect: "mysql",
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
     dialectOptions: {
       ssl: {
         require: true,
